81cbc95136564aa1f871f643d8c073b2684212f9
conrderuben <conrderuben@gmail.com>
2021-04-29 13:23:34 +0200

    Prueba 3

--- src/control/Main.java
+++ src/control/Main.java
@@ -31,7 +31,7 @@ public class Main {
 		
 	public static void main(String[] args) {
 
-		System.out.println("Testtttttttttttttttt");
+		
 		
 		MenuPrincipal.menuprincipal.setVisible(false);
 		MenuInicio.mostrarMenuRegistro(MenuPrincipal.menuprincipal);
